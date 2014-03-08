c15f4ae90704d9af8d679fda9685d3c5757de35b
Jordan Robinson <hypahnova@gmail.com>
2014-03-08 15:23:58 +0000

    More work towards vigenere cipher.
    Adds in decoding, visual frequency output, needs better string combination generator.

--- node/problem/problem.js
+++ node/problem/problem.js
@@ -1,8 +1,77 @@
 //var http = require('http');
 
-function vigenere(key, message) {
+function vigenereEncode(key, message) {
 
     var ret = '';
+
+    key = makeKey(key, message)
+
+    for (var j = 0; j < message.length; j++) {
+        var intChar = ((message.charCodeAt(j) - 65) + (key.charCodeAt(j) - 65)) % 26;
+        ret += String.fromCharCode(intChar + 65);
+        console.log(intChar);
+    }
+
+    return ret;
+}
+
+function vigenereDecode(key, message) {
+    var ret = '';
+
+    key = makeKey(key, message)
+
+    for (var j = 0; j < message.length; j++) {
+        var intChar = 0;
+
+        if (message.charCodeAt(j) - 65 < key.charCodeAt(j) - 65) {
+           intChar = 26;
+        }
+
+        intChar = (message.charCodeAt(j) - key.charCodeAt(j)) + intChar;
+
+        ret += String.fromCharCode(intChar + 65);
+    }
+    return ret;
+}
+
+function vigenereFrequency(message) {
+
+}
+
+function generateCombinations() {
+    var ret = "";
+
+    for (var m = 0; m < 26; m++) {
+        for (var l = 0; l < 26; l++) {
+            for (var j = 0; j < 26; j++) {
+                for (var k = 0; k < 26; k++) {
+                    //for (var i = 0; i < 26; i++) {
+                    //    ret += String.fromCharCode(i + 65);
+                        ret += String.fromCharCode(k + 65);
+                        ret += String.fromCharCode(j + 65);
+                        ret += String.fromCharCode(l + 65);
+                        ret += String.fromCharCode(m + 65);
+                        //console.log(ret)
+                        console.log(getFreq(vigenereDecode(ret, 'KSGDGBJQBEQKKLGDG')));
+                        ret = "";
+                    //}
+                }
+            }
+        }
+    }
+}
+
+function getFreq (input) {
+    var frequency = {};
+    input.split("").forEach(function (position) {
+        frequency[position] = frequency[position] || 0;
+        frequency[position] += 1;
+    });
+
+    return JSON.stringify(frequency) + " -- " + input;
+}
+
+function makeKey(key, message) {
 
     while (key.length < message.length) {
         key = key + key;
@@ -12,16 +81,17 @@ function vigenere(key, message) {
         key = key.substring(0, message.length);
     }
 
-    for (var j = 0; j < message.length; j++) {
-        var intChar = ((message.charCodeAt(j) - 65) + (key.charCodeAt(j) - 65)) % 26;
-        ret += String.fromCharCode(intChar + 65);
-        console.log(intChar);
-    }
-
-    return ret
+    return key;
 }
 
-console.log(vigenere('REDDIT', 'TODAYISMYBIRTHDAY'))
+
+//console.log(vigenereEncode('REDDIT', 'TODAYISMYBIRTHDAY'))
+//console.log(vigenereDecode('REDDIT', 'KSGDGBJQBEQKKLGDG'))
+//console.log(getFreq('BANANA'))
+
+//console.log(vigenereFrequency('KSGDGBJQBEQKKLGDG'))
+generateCombinations()
+
 
 //http.createServer(function (req, res) {
 //    var answer = vigenere();
